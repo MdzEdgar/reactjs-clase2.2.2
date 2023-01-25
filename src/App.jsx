d4bfd185cf5c47f1f17ac9ca3035a40ed619e5b6
Edgar Méndez <mdzredgar@gmail.com>
2023-01-24 21:58:30 -0600

    Información de tarea

--- src/App.jsx
+++ src/App.jsx
@@ -20,6 +20,10 @@ function App() {
   //Ejercicio
   //Mostrar en pantalla la bandera de su país, la población, la capital, region
 
+  //Ejercicio 2
+  //Crear un componente que dependiendo del string(nombre del pais) 
+  //que reciba por prop me renderice la informacion del pais coincidente
+
   return (
     <div className="App">
       {country ? (
