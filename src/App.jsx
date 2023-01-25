774bb13f3c4dcb12ca41d2bfd27e40caec5ed687
Edgar Méndez <mdzredgar@gmail.com>
2023-01-24 21:54:50 -0600

    Terminado el ejercicio de clase

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import axios from 'axios'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
 
+  const [country, setCountry] = useState()
+
   const getInfoCountry = () => {
     const URL ="https://restcountries.com/v3.1/name/mexico" 
     axios.get(URL)
-    .then((res) => console.log(res.data))
+    .then((res) => setCountry(res.data[0]))
     .catch((err) => console.log(err))
   }
 
@@ -15,11 +17,29 @@ function App() {
     getInfoCountry();
   }, [])
 
+  //Ejercicio
+  //Mostrar en pantalla la bandera de su país, la población, la capital, region
+
   return (
     <div className="App">
-     <h1>Hola Mundo</h1>
+      {country ? (
+        <>
+          <article className='cardCountry'>
+            <div className='cardCountry__img'>
+              <img src={country.flags.svg} alt="" />
+            </div>
+            <ul className='cardCountry__list'>
+              <li><b>Población: </b>{country.population}</li>
+              <li><b>Capital: </b>{country.capital}</li>
+              <li><b>Región: </b>{country.region}</li>
+            </ul>
+          </article>
+        </>
+      ) : (
+        <h1>Cargando...</h1>
+      )}
     </div>
-  )
+  );
 }
 
 export default App
